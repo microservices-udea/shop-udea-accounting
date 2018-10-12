e3022f371ae102dc0c7988b6f660f9444e0e7293
JSteven <jstevenyepes@gmail.com>
2018-10-11 21:44:47 -0500

    [Fix] Spring kafka doesn't consume messages

--- src/main/java/com/udea/edu/co/microservices/accounting/Infraestructure/KafkaOrderPaidEvent.java
+++ src/main/java/com/udea/edu/co/microservices/accounting/Infraestructure/KafkaOrderPaidEvent.java
@@ -2,16 +2,23 @@ package com.udea.edu.co.microservices.accounting.Infraestructure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import com.udea.edu.co.microservices.accounting.entities.Account;
 import com.udea.edu.co.microservices.accounting.entities.AccountNumber;
 import com.udea.edu.co.microservices.accounting.entities.EventMessage;
 import com.udea.edu.co.microservices.accounting.repository.AccountsRepository;
 import com.udea.edu.co.microservices.accounting.repository.EventMessagessRepository;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
+@Component
 public class KafkaOrderPaidEvent implements EventConsumer {
 
     @Autowired
@@ -26,11 +33,17 @@ public class KafkaOrderPaidEvent implements EventConsumer {
     String assetsAccountingClass = "02";
 
     @Override
-    //@KafkaListener(topics = "orderTopic")
+    @KafkaListener(topics = "${topic.orders}")
     public void processEvent(String message) {
         System.out.println("event:" + message);
         // filter message before to transform
-        String eventType = JsonPath.read(message, jsonPathEventTypeExp);
+
+        String eventType = "";
+        try {
+            eventType = JsonPath.read(message, jsonPathEventTypeExp);
+        } catch (PathNotFoundException e){
+            e.printStackTrace();
+        }
 
         if (!"".equals(eventType) && eventOrderPaid.equals(eventType)) {
             String uuid = JsonPath.read(message, jsonPathUuidExp);
@@ -59,4 +72,6 @@ public class KafkaOrderPaidEvent implements EventConsumer {
         }
 
     }
+
+
 }
